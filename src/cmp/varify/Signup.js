8cf5643450e7f398aa7756e13d43209f13c2b780
2M infotech <2minfotech@2Ms-Mac-mini.local>
2021-01-23 19:14:41 +0530

    first commit

--- src/cmp/varify/Signup.js
+++ src/cmp/varify/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Signup() {
     const [formData, setFromData] = useState({
@@ -7,6 +7,18 @@ function Signup() {
         mName: '',
         age: '',
         gender: '',
+        qualification: '',
+        email: '',
+        password: '',
+        cPassword: ''
+    });
+    const [errorData, setErrorData] = useState({
+        fName: '',
+        lName: '',
+        mName: '',
+        age: '',
+        gender: '',
+        qualification: '',
         email: '',
         password: '',
         cPassword: ''
@@ -15,29 +27,169 @@ function Signup() {
     const handleOnChange = (e) => {
         setFromData({ ...formData, [e.target.name]: e.target.value })
     }
-    console.log("formData ", formData);
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        localStorage.setItem('Users', JSON.stringify(formData));
+        console.log("onSubimt=======> ", formData);
+        handleReSetForm()
+    }
+
+    const handleReSetForm = () => {
+        setFromData({
+            fName: '',
+            lName: '',
+            mName: '',
+            age: '',
+            gender: '',
+            qualification: '',
+            email: '',
+            password: '',
+            cPassword: ''
+        })
+    }
+
+    const handleCheckPassword = () => {
+        var paswd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+        if (formData.password.match(paswd)) {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['password']: 'password should have uppercase, lowercase, char and number ' })
+            return false;
+        }
+    }
+
+    const handleInvalidError = () => {
+        var eml = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if (formData.email.match(eml)) {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['password']: 'please enter valid email id ' })
+            return false;
+        }
+    }
+
+    const handleConfirmPassword = () => {
+        if (formData.password === formData.cPassword) {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['cPassword']: 'password and confirm password not match ' })
+            return false;
+        }
+    }
+
+    const handleFillFNameAttr = () => {
+        if (formData.fName !== '') {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['fName']: 'must fill this field' })
+        }
+    }
+    
+    const handleFillLNameAttr = () => {
+        if (formData.lName !== '') {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['lName']: 'must fill this field' })
+        }
+    } 
+    
+    const handleCheckAge = () => {
+        if (formData.age < 109 && formData.age > 0) {
+            return true;
+        }
+        else {
+            setErrorData({ ...errorData, ['age']: 'password should have uppercase, lowercase, char and number ' })
+            return false;
+        }
+    }
 
     return (
-        <div>
-            <form>
-                <p>First Name :</p> <input type="text" placeholder="First Name" name="fName" value={formData && formData.fName || ''} onChange={handleOnChange} /> <br />
-                <p>Last Name :</p> <input type="text" placeholder="Last Name" name="lName" value={formData && formData.lName || ''} onChange={handleOnChange} /> <br />
-                <p>Middle Name :</p> <input type="text" placeholder="Middle Name" name="mName" value={formData && formData.mName || ''} onChange={handleOnChange} /> <br />
-                <p>Age :</p> <input type="number" placeholder="Age" name="age" value={formData && formData.age || ''} onChange={handleOnChange} /> <br />
-                <div>
-                    <p>Gender :</p>
-                    <input type="radio" name="gender" value="male" name="gender" value={formData && formData.gender || ''} onChange={handleOnChange} />Male
-                    <input type="radio" name="gender" value="female" name="gender" value={formData && formData.gender || ''} onChange={handleOnChange} /> Female<br />
-                </div>
-                <p>Email :</p> <input type="email" placeholder="Email" name="email" value={formData && formData.email || ''} onChange={handleOnChange} /> <br />
-                <p>Password :</p> <input type="password" placeholder="Password" /> <br />
-                <p>Confirm Password :</p> <input type="password" placeholder="Confirm Password" /> <br /><br />
-                <button>Submit</button>
+        <div style={{ marginLeft: '20%', marginBottom: '10%' }}>
+            <form onSubmit={handleOnSubmit}>
+                <label>
+                    First Name :
+                    <input type="text" placeholder="First Name" name="fName" value={formData && formData.fName || ''} onChange={handleOnChange} onBlur={(value) => handleFillFNameAttr(value)} />
+                    {
+                        (errorData && errorData.fName) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.fName}</div>
+                        )
+                    }
+                </label> <br />
+                <label>
+                    Last Name :
+                    <input type="text" placeholder="Last Name" name="lName" value={formData && formData.lName || ''} onChange={handleOnChange} onBlur={(value) => handleFillLNameAttr(value)} />
+                    {
+                        (errorData && errorData.lName) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.lName}</div>
+                        )
+                    }
+                </label><br />
+                <label>
+                    Middle Name :
+                    <input type="text" placeholder="Middle Name" name="mName" value={formData && formData.mName || ''} onChange={handleOnChange} />
+                </label> <br />
+                <label>
+                    Age :
+                    <input type="text" placeholder="Age" name="age" value={formData && formData.age || ''} onChange={handleOnChange} onBlur={(value) => handleCheckAge(value)} />
+                    {
+                        (errorData && errorData.age) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.age}</div>
+                        )
+                    }
+                </label><br />
+                <label>
+                    Gender :
+                    <input type="radio" name="gender" value="male" checked={formData && formData.gender === "male" || ''} onChange={handleOnChange} />Male
+                    <input type="radio" name="gender" value="female" checked={formData && formData.gender === "female" || ''} onChange={handleOnChange} /> Female
+                </label> <br />
+                <label>
+                    Qualification :
+                    <input type="checkbox" name="qualification" value="school" onChange={handleOnChange} /> School
+                    <input type="checkbox" name="qualification" value="bachelor" onChange={handleOnChange} /> Bachelor
+                    <input type="checkbox" name="qualification" value="masters" onChange={handleOnChange} /> Masters
+                </label> <br />
+                <label>
+                    Email :
+                    <input type="email" placeholder="Email" name="email" value={formData && formData.email || ''} onChange={handleOnChange} onBlur={(value) => handleInvalidError(value)} />
+                    {
+                        (errorData && errorData.email) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.email}</div>
+                        )
+                    }
+                </label><br />
+                <label>
+                    Password :
+                    <input type="password" placeholder="Password" name="password" value={formData && formData.password || ''} onChange={handleOnChange} onBlur={(value) => handleCheckPassword(value)} />
+                    {
+                        (errorData && errorData.password) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.password}</div>
+                        )
+                    }
+
+                </label> <br />
+                <label>
+                    Confirm Password :
+                    <input type="password" placeholder="Confirm Password" name="cPassword" value={formData && formData.cPassword || ''} onChange={handleOnChange} onBlur={(value) => handleConfirmPassword(value)} />
+                    {
+                        (errorData && errorData.cPassword) && (
+                            <div style={{ color: 'red' }}>{errorData && errorData.cPassword}</div>
+                        )
+                    }
+                </label> <br />
+                <br />
+                <button type="submit" value="Submit" >Submit </button>
+                <button type="reset" onClick={handleReSetForm}>Clear </button>
             </form>
         </div>
     )
 }
 export default Signup
 
-//<input type="radio" name="gender" value="male" checked> Male<br>
-//<input type="radio" name="gender" value="female"> Female<br>
+//{(value)=> CheckPassword(value)}
